3205ee878ba5203c2a2af48c7964d64a379301c8
dgrujic <dgrujic@users.noreply.github.com>
2026-06-01 12:32:40 +0000

    deploy: f3f5fc70a85439d87d243d49b8c6d81e9249232a

--- doc/workflow/searcher-09f2665d.js
+++ doc/workflow/searcher-09f2665d.js
@@ -440,7 +440,7 @@ window.search = window.search || {};
         if (yes) {
             loadSearchScript(
                 window.path_to_searchindex_js ||
-                path_to_root + 'searchindex-2846c8cf.js',
+                path_to_root + 'searchindex-d20ced3d.js',
                 'mdbook-search-index');
             search_wrap.classList.remove('hidden');
             searchicon.setAttribute('aria-expanded', 'true');
