9e2df84884ecbc5ba17e144c3284e8e825a30072
Dusan <dusgru@gmail.com>
2026-08-04 23:18:22 +0200

    Added NOR4N1_D* cells.

---
+++ cells/NOR4N1_D4/NOR4N1_D4.py
@@ -0,0 +1,75 @@
+########################################################################
+#
+# Copyright 2026 Dr. Dušan Grujić (dusan.grujic@etf.bg.ac.rs)
+# 
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+# 
+#    https://www.apache.org/licenses/LICENSE-2.0
+# 
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+########################################################################
+
+from glow_parcells import *
+from glow_utils.symsim import Symsim
+from glow_utils.symtech import SymTech
+from sympy import Nor, Not
+from sympy.abc import x, y, z, w
+
+def info():
+    """
+    Returns a dictionary with cell information
+    Key         Value
+    name        Cell name
+    pinList     List of cell pins
+    description Cell description
+    """
+    cellInfo = { 'name' : 'NOR4N1_D4',
+                 'pinList' : ['AN', 'B', 'C', 'D', 'Y', 'VDD', 'VSS'],
+                 'description' : 'NOR4 with inverted input A and drive strength x4'
+    }
+    return cellInfo
+
+def generate(genFlat = True, anonimize = True):
+    """
+    Generate the circuit structure.
+    If genFlat = True generate a flat circuit with suffix _flat
+    If anonimize = True anonimize devices and nodes in the generated flat circuit
+    """
+    cellInfo = info()
+
+    NOR4N1_D4 = Symsubcircuit(cellInfo['name'], cellInfo['pinList'])
+    nor4_pins = nor4_par.getTerminals()
+    wn = SymTech.technology['nor4x4WN']
+    wp = SymTech.technology['nor4x4WP']
+    ngp = 3
+    ngn = 2
+    nor4 = nor4_par('nor4', nor4_pins, {'WP' : wp, 'NGP' : ngp, 'WN' : wn, 'NGN' : ngn})
+    inv_wn = 890e-9
+    inv_wp = 1400e-9
+    inv_inst = inv_par('inv_inst', ['AN', 'A', 'VDD', 'VSS'], {'WN' : inv_wn, 'WP' : inv_wp})
+    NOR4N1_D4.addElement([inv_inst, nor4])
+
+    # Flatten the circuit
+    if genFlat:
+        NOR4N1_D4_flat = NOR4N1_D4.flat()
+    if anonimize:
+        NOR4N1_D4_flat.anonimize()
+
+def check(verbose = False):
+    """
+    Check if the circuit works as expected
+    """
+    expectedFns = [ Nor(Not(x), y, z, w) ]
+    cellInfo = info()
+    name = cellInfo["name"]
+    allCircuits = Symsubcircuit.getSubckts()
+    circuit = allCircuits[ name + "_flat" ]
+    sim = Symsim(circuit, verbose = verbose)
+    return sim.combCheck(expectedFns)
---
+++ cells/NOR4N1_DL/NOR4N1_DL.py
@@ -0,0 +1,73 @@
+########################################################################
+#
+# Copyright 2026 Dr. Dušan Grujić (dusan.grujic@etf.bg.ac.rs)
+# 
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+# 
+#    https://www.apache.org/licenses/LICENSE-2.0
+# 
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+########################################################################
+
+from glow_parcells import *
+from glow_utils.symsim import Symsim
+from glow_utils.symtech import SymTech
+from sympy import Nor, Not
+from sympy.abc import x, y, z, w
+
+def info():
+    """
+    Returns a dictionary with cell information
+    Key         Value
+    name        Cell name
+    pinList     List of cell pins
+    description Cell description
+    """
+    cellInfo = { 'name' : 'NOR4N1_DL',
+                 'pinList' : ['AN', 'B', 'C', 'D', 'Y', 'VDD', 'VSS'],
+                 'description' : 'NOR4 with inverted input A and weak drive strength'
+    }
+    return cellInfo
+
+def generate(genFlat = True, anonimize = True):
+    """
+    Generate the circuit structure.
+    If genFlat = True generate a flat circuit with suffix _flat
+    If anonimize = True anonimize devices and nodes in the generated flat circuit
+    """
+    cellInfo = info()
+
+    NOR4N1_DL = Symsubcircuit(cellInfo['name'], cellInfo['pinList'])
+    nor4_pins = nor4_par.getTerminals()
+    wp = 600e-9
+    wn = 200e-9
+    nor4 = nor4_par('nor4', nor4_pins, {'WP' : wp, 'WN' : wn})
+    inv_wn = 150e-9
+    inv_wp = 230e-9
+    inv_inst = inv_par('inv_inst', ['AN', 'A', 'VDD', 'VSS'], {'WN' : inv_wn, 'WP' : inv_wp})
+    NOR4N1_DL.addElement([inv_inst, nor4])
+
+    # Flatten the circuit
+    if genFlat:
+        NOR4N1_DL_flat = NOR4N1_DL.flat()
+    if anonimize:
+        NOR4N1_DL_flat.anonimize()
+
+def check(verbose = False):
+    """
+    Check if the circuit works as expected
+    """
+    expectedFns = [ Nor(Not(x), y, z, w) ]
+    cellInfo = info()
+    name = cellInfo["name"]
+    allCircuits = Symsubcircuit.getSubckts()
+    circuit = allCircuits[ name + "_flat" ]
+    sim = Symsim(circuit, verbose = verbose)
+    return sim.combCheck(expectedFns)
