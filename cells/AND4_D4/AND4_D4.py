e20d1a165d823ef98454b424689e1a74cd83fe5a
Dusan <dusgru@gmail.com>
2026-08-05 20:34:08 +0200

    Added AND4_D* cells.

---
+++ cells/AND4_D4/AND4_D4.py
@@ -0,0 +1,80 @@
+########################################################################
+#
+# Copyright 2026 Dr. Dušan Grujić (dusan.grujic@etf.bg.ac.rs)
+# 
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+# 
+#    https://www.apache.org/licenses/LICENSE-2.0
+# 
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+########################################################################
+
+from pathlib import Path
+import importlib
+import os
+import sys
+
+from glow_parcells import *
+from glow_utils.symsim import Symsim
+from glow_utils.symtech import SymTech
+from sympy import And
+from sympy.abc import x, y, z, w
+
+def info():
+    """
+    Returns a dictionary with cell information
+    Key         Value
+    name        Cell name
+    pinList     List of cell pins
+    description Cell description
+    """
+    cellInfo = { 'name' : 'AND4_D4',
+                 'pinList' : ['A', 'B', 'C', 'D', 'Y', 'VDD', 'VSS'],
+                 'description' : 'AND4 with drive strength x4'
+    }
+    return cellInfo
+
+def generate(genFlat = True, anonimize = True):
+    """
+    Generate the circuit structure.
+    If genFlat = True generate a flat circuit with suffix _flat
+    If anonimize = True anonimize devices and nodes in the generated flat circuit
+    """
+    cellInfo = info()
+
+    AND4_D4 = Symsubcircuit(cellInfo['name'], cellInfo['pinList'])
+    wn = 720e-9
+    wp = 620e-9
+    nand4_inst1 = nand4_par('nand4_inst1', ['A', 'B', 'C', 'D', 'NO', 'VDD', 'VSS'], {'WN' : wn, 'WP' : wp})
+    nand4_inst2 = nand4_par('nand4_inst2', ['A', 'B', 'C', 'D', 'NO', 'VDD', 'VSS'], {'WN' : wn, 'WP' : wp})
+    inv_wn = SymTech.technology['invx2WN']
+    inv_wp = SymTech.technology['invx2WP']
+    ng = 2
+    inv_inst = inv_par('inv_inst', ['NO', 'Y', 'VDD', 'VSS'], {'WN' : ng * inv_wn, 'WP' : ng * inv_wp, 'NGN' : ng, 'NGP' : ng})
+    AND4_D4.addElement([nand4_inst1, nand4_inst2, inv_inst])
+
+    # Flatten the circuit
+    if genFlat:
+        AND4_D4_flat = AND4_D4.flat()
+    if anonimize:
+        AND4_D4_flat.anonimize()
+
+def check(verbose = False):
+    """
+    Check if the circuit works as expected
+    """
+    expectedFns = [ And(x, y, z, w) ]
+    cellInfo = info()
+    name = cellInfo["name"]
+    allCircuits = Symsubcircuit.getSubckts()
+    circuit = allCircuits[ name + "_flat" ]
+    sim = Symsim(circuit, verbose = verbose)
+    return sim.combCheck(expectedFns)
+
