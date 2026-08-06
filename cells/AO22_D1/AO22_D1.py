e8ccf481df283f342a9d6091d3f5fc0627a43e5d
Dusan <dusgru@gmail.com>
2026-08-06 14:08:36 +0200

    Added AO22_D* cells.

---
+++ cells/AO22_D1/AO22_D1.py
@@ -0,0 +1,87 @@
+########################################################################
+#
+# Copyright 2026 Dr. Dušan Grujić (dusan.grujic@etf.bg.ac.rs)
+# 
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+# 
+#    https://www.apache.org/licenses/LICENSE-2.0
+# 
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+########################################################################
+
+from glow_parcells import *
+from glow_utils.symsim import Symsim
+from glow_utils.symtech import SymTech
+from glow_utils.symmosfet import SymNMOS, SymPMOS
+from sympy import And, Or
+from sympy.abc import x, y, z, w
+
+def info():
+    """
+    Returns a dictionary with cell information
+    Key         Value
+    name        Cell name
+    pinList     List of cell pins
+    description Cell description
+    """
+    cellInfo = { 'name' : 'AO22_D1',
+                 'pinList' : ['A', 'B', 'C', 'D', 'Y', 'VDD', 'VSS'],
+                 'description' : 'AO22 with drive strength x1'
+    }
+    return cellInfo
+
+def generate(genFlat = True, anonimize = True):
+    """
+    Generate the circuit structure.
+    If genFlat = True generate a flat circuit with suffix _flat
+    If anonimize = True anonimize devices and nodes in the generated flat circuit
+    """
+    cellInfo = info()
+
+    AO22_D1 = Symsubcircuit(cellInfo['name'], cellInfo['pinList'])
+    wn = 300e-9
+    wp = 400e-9
+    lmin = SymTech.technology['Lmin']
+
+    # Pull-down network
+    n0 = SymNMOS("N0", ['n0', 'A', 'VSS', 'VSS'], {'w' : wn, 'l' : lmin })
+    n1 = SymNMOS("N1", ['NY', 'B', 'n0', 'VSS'], {'w' : wn, 'l' : lmin })
+    n2 = SymNMOS("N2", ['n1', 'C', 'VSS', 'VSS'], {'w' : wn, 'l' : lmin })
+    n3 = SymNMOS("N3", ['NY', 'D', 'n1', 'VSS'], {'w' : wn, 'l' : lmin })
+
+    # Pull-up network
+    p0 = SymPMOS("P0", ['n2', 'A', 'VDD', 'VDD'], {'w' : wp, 'l' : lmin })
+    p1 = SymPMOS("P1", ['n2', 'B', 'VDD', 'VDD'], {'w' : wp, 'l' : lmin })
+    p2 = SymPMOS("P2", ['NY', 'C', 'n2', 'VDD'], {'w' : wp, 'l' : lmin })
+    p3 = SymPMOS("P3", ['NY', 'D', 'n2', 'VDD'], {'w' : wp, 'l' : lmin })
+
+    inv_wn = SymTech.technology['invx1WN']
+    inv_wp = SymTech.technology['invx1WP']
+    inv_inst = inv_par('inv_inst', ['NY', 'Y', 'VDD', 'VSS'], {'WN' : inv_wn, 'WP' : inv_wp})
+    AO22_D1.addElement([n0, n1, n2, n3, p0, p1, p2, p3, inv_inst])
+
+    # Flatten the circuit
+    if genFlat:
+        AO22_D1_flat = AO22_D1.flat()
+    if anonimize:
+        AO22_D1_flat.anonimize()
+
+def check(verbose = False):
+    """
+    Check if the circuit works as expected
+    """
+    expectedFns = [ Or(And(x, y), And(z, w)) ]
+    cellInfo = info()
+    name = cellInfo["name"]
+    allCircuits = Symsubcircuit.getSubckts()
+    circuit = allCircuits[ name + "_flat" ]
+    sim = Symsim(circuit, verbose = verbose)
+    return sim.combCheck(expectedFns)
+
