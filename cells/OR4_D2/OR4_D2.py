4e25d0cb6d14734357e0bda154db49822f49253b
Dusan <dusgru@gmail.com>
2026-08-07 14:00:24 +0200

    Added OR4_D*, XOR2_D* and XNOR2_D* cells.

---
+++ cells/OR4_D2/OR4_D2.py
@@ -0,0 +1,72 @@
+########################################################################
+#
+# Copyright 2026 Dr. Dušan Grujić (dusan.grujic@etf.bg.ac.rs)
+# 
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+# 
+#    https://www.apache.org/licenses/LICENSE-2.0
+# 
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+########################################################################
+
+from glow_parcells import *
+from glow_utils.symsim import Symsim
+from glow_utils.symtech import SymTech
+from sympy import Or
+from sympy.abc import x, y, z, w
+
+def info():
+    """
+    Returns a dictionary with cell information
+    Key         Value
+    name        Cell name
+    pinList     List of cell pins
+    description Cell description
+    """
+    cellInfo = { 'name' : 'OR4_D2',
+                 'pinList' : ['A', 'B', 'C', 'D', 'Y', 'VDD', 'VSS'],
+                 'description' : 'OR4 with drive strength x2'
+    }
+    return cellInfo
+
+def generate(genFlat = True, anonimize = True):
+    """
+    Generate the circuit structure.
+    If genFlat = True generate a flat circuit with suffix _flat
+    If anonimize = True anonimize devices and nodes in the generated flat circuit
+    """
+    cellInfo = info()
+
+    OR4_D2 = Symsubcircuit(cellInfo['name'], cellInfo['pinList'])
+    wn = 500e-9
+    wp = 1300e-9
+    nor4_inst = nor4_par('nor4', ['A', 'B', 'C', 'D', 'NO', 'VDD', 'VSS'], {'WN' : wn, 'WP' : wp})
+    inv_wn = SymTech.technology['invx2WN']
+    inv_wp = SymTech.technology['invx2WP']
+    inv_inst = inv_par('inv_inst', ['NO', 'Y', 'VDD', 'VSS'], {'WN' : inv_wn, 'WP' : inv_wp})
+    OR4_D2.addElement([nor4_inst, inv_inst])
+
+    # Flatten the circuit
+    if genFlat:
+        OR4_D2_flat = OR4_D2.flat()
+    if anonimize:
+        OR4_D2_flat.anonimize()
+
+def check(verbose = False):
+    """
+    Check if the circuit works as expected
+    """
+    expectedFns = [ Or(x, y, z, w) ]
+    cellInfo = info()
+    name = cellInfo["name"]
+    allCircuits = Symsubcircuit.getSubckts()
+    circuit = allCircuits[ name + "_flat" ]
+    sim = Symsim(circuit, verbose = verbose)
+    return sim.combCheck(expectedFns)
---
+++ cells/XNOR2_D1/XNOR2_D1.py
@@ -0,0 +1,85 @@
+########################################################################
+#
+# Copyright 2026 Dr. Dušan Grujić (dusan.grujic@etf.bg.ac.rs)
+# 
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+# 
+#    https://www.apache.org/licenses/LICENSE-2.0
+# 
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+########################################################################
+
+from glow_parcells import *
+from glow_utils.symsim import Symsim
+from glow_utils.symtech import SymTech
+from glow_utils.symmosfet import SymNMOS, SymPMOS
+from sympy import Xor, Not
+from sympy.abc import x, y
+
+def info():
+    """
+    Returns a dictionary with cell information
+    Key         Value
+    name        Cell name
+    pinList     List of cell pins
+    description Cell description
+    """
+    cellInfo = { 'name' : 'XNOR2_D1',
+                 'pinList' : ['A', 'B', 'Y', 'VDD', 'VSS'],
+                 'description' : 'XNOR2 with drive strength x1'
+    }
+    return cellInfo
+
+def generate(genFlat = True, anonimize = True):
+    """
+    Generate the circuit structure.
+    If genFlat = True generate a flat circuit with suffix _flat
+    If anonimize = True anonimize devices and nodes in the generated flat circuit
+    """
+    cellInfo = info()
+
+    XNOR2_D1 = Symsubcircuit(cellInfo['name'], cellInfo['pinList'])
+    lmin = SymTech.technology['Lmin']
+    wn = 500e-9
+    wp = 800e-9
+
+    wnmin = 200e-9
+    wpmin = 300e-9
+
+    # NMOS
+    n0 = SymNMOS("N0", ['BN', 'AN', 'Y', 'VSS'], {'w' : wn, 'l' : lmin })
+    n1 = SymNMOS("N1", ['BI', 'A', 'Y', 'VSS'], {'w' : wn, 'l' : lmin })
+
+    # PMOS
+    p0 = SymPMOS("P0", ['BN', 'A', 'Y', 'VDD'], {'w' : wp, 'l' : lmin })
+    p1 = SymPMOS("P1", ['BI', 'AN', 'Y', 'VDD'], {'w' : wp, 'l' : lmin })
+
+    inv_inst1 = inv_par('inv_inst', ['BN', 'BI', 'VDD', 'VSS'], {'WN' : wn, 'WP' : wp})
+    inv_inst2 = inv_par('inv_inst', ['B', 'BN', 'VDD', 'VSS'], {'WN' : wn, 'WP' : wp})
+    inv_inst3 = inv_par('inv_inst', ['A', 'AN', 'VDD', 'VSS'], {'WN' : wnmin, 'WP' : wpmin})
+    XNOR2_D1.addElement([inv_inst1, inv_inst2, inv_inst3, n0, n1, p0, p1])
+
+    # Flatten the circuit
+    if genFlat:
+        XNOR2_D1_flat = XNOR2_D1.flat()
+    if anonimize:
+        XNOR2_D1_flat.anonimize()
+
+def check(verbose = False):
+    """
+    Check if the circuit works as expected
+    """
+    expectedFns = [ Not(Xor(x, y)) ]
+    cellInfo = info()
+    name = cellInfo["name"]
+    allCircuits = Symsubcircuit.getSubckts()
+    circuit = allCircuits[ name + "_flat" ]
+    sim = Symsim(circuit, verbose = verbose)
+    return sim.combCheck(expectedFns)
