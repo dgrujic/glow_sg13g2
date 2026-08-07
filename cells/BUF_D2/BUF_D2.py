59d067e54e6a61092b5731468ebc7cf66f98df56
Dusan <dusgru@gmail.com>
2026-08-07 16:38:43 +0200

    Added cells BUF_D*.

---
+++ cells/BUF_D2/BUF_D2.py
@@ -0,0 +1,73 @@
+########################################################################
+#
+# Copyright 2026 Dr. Dušan Grujić (dusan.grujic@etf.bg.ac.rs)
+# 
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+# 
+#    https://www.apache.org/licenses/LICENSE-2.0
+# 
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+########################################################################
+
+from glow_parcells import *
+from glow_utils.symsim import Symsim
+from glow_utils.symtech import SymTech
+from sympy.abc import x
+
+def info():
+    """
+    Returns a dictionary with cell information
+    Key         Value
+    name        Cell name
+    pinList     List of cell pins
+    description Cell description
+    """
+    cellInfo = { 'name' : 'BUF_D2',
+                 'pinList' : ['A', 'Y', 'VDD', 'VSS'],
+                 'description' : 'Buffer with drive strength x2'
+    }
+    return cellInfo
+
+def generate(genFlat = True, anonimize = True):
+    """
+    Generate the circuit structure.
+    If genFlat = True generate a flat circuit with suffix _flat
+    If anonimize = True anonimize devices and nodes in the generated flat circuit
+    """
+    cellInfo = info()
+    wnin = 2 * 200e-9
+    wpin = 2 * 300e-9
+
+    wnout = SymTech.technology['invx2WN']
+    wpout = SymTech.technology['invx2WP']
+
+    BUF_D2 = Symsubcircuit(cellInfo['name'], cellInfo['pinList'])
+    inv1 = inv_par('inv1', ['A', 'AN', 'VDD', 'VSS'], {'WN' : wnin, 'WP' : wpin})
+    inv2 = inv_par('inv2', ['AN', 'Y', 'VDD', 'VSS'], {'WN' : wnout, 'WP' : wpout})
+    BUF_D2.addElement([inv1, inv2])
+
+    # Flatten the circuit
+    if genFlat:
+        BUF_D2_flat = BUF_D2.flat()
+    if anonimize:
+        BUF_D2_flat.anonimize()
+
+def check(verbose = False):
+    """
+    Check if the circuit works as expected
+    """
+    expectedFns = [ x ]
+    cellInfo = info()
+    name = cellInfo["name"]
+    allCircuits = Symsubcircuit.getSubckts()
+    circuit = allCircuits[ name + "_flat" ]
+    sim = Symsim(circuit, verbose = verbose)
+    return sim.combCheck(expectedFns)
+
